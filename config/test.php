5ca74aab2d17c95ece13f13f90c9371a57f75cbc
Pavel Klochkov <pv.klochkov@gmail.com>
2024-06-17 00:13:39 +0300

    Revert "оптимизация настроек"
    This reverts commit b98ee246c03c696a3c128edfc4fced008e163084.

--- config/test.php
+++ config/test.php
@@ -1,35 +1,38 @@
 <?php
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/test_db.php';
 
 /**
  * Application configuration shared by all test types
  */
 return [
     'id' => 'basic-tests',
-
-    'language'   => 'en-US',
+    'basePath' => dirname(__DIR__),
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'language' => 'en-US',
     'components' => [
-        'db'           => [
-            'dsn'               => 'sqlite:@app/sqlite-test.db',
-            'enableSchemaCache' => true,
-        ],
-        'mailer'       => [
-            'class'            => \yii\symfonymailer\Mailer::class,
-            'viewPath'         => '@app/mail',
+        'db' => $db,
+        'mailer' => [
+            'class' => \yii\symfonymailer\Mailer::class,
+            'viewPath' => '@app/mail',
             // send all mails to a file by default.
             'useFileTransport' => true,
-            'messageClass'     => 'yii\symfonymailer\Message'
+            'messageClass' => 'yii\symfonymailer\Message'
         ],
         'assetManager' => [
             'basePath' => __DIR__ . '/../web/assets',
         ],
-        'urlManager'   => [
+        'urlManager' => [
             'showScriptName' => true,
         ],
-        'user'         => [
+        'user' => [
             'identityClass' => 'app\models\User',
         ],
-        'request'      => [
-            'cookieValidationKey'  => 'test',
+        'request' => [
+            'cookieValidationKey' => 'test',
             'enableCsrfValidation' => false,
             // but if you absolutely need it set cookie domain to localhost
             /*
@@ -39,4 +42,5 @@ return [
             */
         ],
     ],
+    'params' => $params,
 ];
--- config/web.php
+++ config/web.php
@@ -1,28 +1,47 @@
 <?php
 
+$params = require __DIR__ . '/params.php';
+$db = require __DIR__ . '/db.php';
+
 $config = [
-    'id'         => 'basic',
+    'id' => 'basic',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
     'components' => [
         'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => 'B483_ama0HlTkwZjdMZ4PRHfpx-7UhK-',
         ],
-
-        'user'         => [
-            'identityClass'   => 'app\models\User',
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'user' => [
+            'identityClass' => 'app\models\User',
             'enableAutoLogin' => true,
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        'mailer'       => [
-            'class'            => \yii\symfonymailer\Mailer::class,
-            'viewPath'         => '@app/mail',
+        'mailer' => [
+            'class' => \yii\symfonymailer\Mailer::class,
+            'viewPath' => '@app/mail',
             // send all mails to a file by default.
             'useFileTransport' => true,
         ],
-        'log'          => [
+        'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
         ],
+        'db' => $db,
         /*
         'urlManager' => [
             'enablePrettyUrl' => true,
@@ -32,6 +51,7 @@ $config = [
         ],
         */
     ],
+    'params' => $params,
 ];
 
 if (YII_ENV_DEV) {
