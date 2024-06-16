898d4a87f6f6d61da6778883561c64a42c3b4a3e
Pavel Klochkov <pv.klochkov@gmail.com>
2024-06-17 00:13:39 +0300

    Revert "urlManager"
    This reverts commit 4290be7cfcc59ab6f9b2697066e3a4af4d7b6a60.

--- config/web.php
+++ config/web.php
@@ -23,22 +23,14 @@ $config = [
         'log'          => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
         ],
-
+        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
-            'showScriptName'  => false,
-            'rules'           => [
-                '<_c:[\w\-]+>/<id:\d+>'              => '<_c>/view',
-                '<_c:[\w\-]+>'                       => '<_c>/index',
-                '<_c:[\w\-]+>/<_a:[\w\-]+>/<id:\d+>' => '<_c>/<_a>',
-            ],
-            'cache' => [
-                'class' => 'yii\caching\FileCache',
-                'cachePath' => '@app/cache/urlManager',
-                'defaultDuration' => 60
+            'showScriptName' => false,
+            'rules' => [
             ],
         ],
-
+        */
     ],
 ];
 
