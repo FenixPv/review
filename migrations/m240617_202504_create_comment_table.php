a5fd82fc50b30dab9da00eee4d292437662535d3
Pavel Klochkov <pv.klochkov@gmail.com>
2024-06-17 23:33:32 +0300

    migration create_comment_table

---
+++ migrations/m240617_202504_create_comment_table.php
@@ -0,0 +1,54 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%comment}}`.
+ */
+class m240617_202504_create_comment_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp(): void
+    {
+        $this->createTable('{{%comment}}', [
+            'id'         => $this->primaryKey(),
+            'user_id'    => $this->integer()->notNull(),
+            'company_id' => $this->integer()->notNull(),
+            'star'       => $this->integer()->notNull(),
+            'body'       => $this->text(),
+            'FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE',
+            'FOREIGN KEY(company_id) REFERENCES company(id) ON DELETE CASCADE',
+        ]);
+
+        $this->createIndex(
+            'idx-comment',
+            'comment',
+            'id'
+        );
+        $this->createIndex(
+            'idx-user',
+            'user',
+            'id'
+        );
+        $this->createIndex(
+            'idx-user_id',
+            'comment',
+            'user_id'
+        );
+        $this->createIndex(
+            'idx-company_id',
+            'comment',
+            'company_id'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown(): void
+    {
+        $this->dropTable('{{%comment}}');
+    }
+}
